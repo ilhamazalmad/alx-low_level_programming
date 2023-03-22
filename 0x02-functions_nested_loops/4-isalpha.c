0ec639e67ec9e32ff1c4bd070170002eabeb28d0
ilhamazalmad <ilhamazalmad11@gmail.com>
2023-03-22 09:00:44 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ *@c : the char to test if is lower
+ *
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c))
+		return (1);
+	else
+		return (0);
+}
