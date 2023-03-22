8160ad70a2ada066123700040ac01ce7c792fbdb
ilhamazalmad <ilhamazalmad11@gmail.com>
2023-03-22 07:48:52 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,11 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char text[] = "_putchar";
+	int i;
+
+	for (i = 0; i < (int)sizeof(text); i++)
+		_putchar(text[i]);
 	_putchar('\n');
 	return (0);
 }
