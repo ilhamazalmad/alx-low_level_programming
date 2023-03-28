b86753379b27b8ec906cd5c39b20d41c1bcaeff9
ilhamazalmad <ilhamazalmad11@gmail.com>
2023-03-28 08:15:46 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - returns the length of a string.
+ *  @s : first param
+ *
+ * Return: strlen of the string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,7 @@
 #include <ctype.h>
 #include <unistd.h>
+#include <string.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
