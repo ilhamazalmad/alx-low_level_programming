231b21ccc3b37d4982e323b0317c300ec2741551
ilhamazalmad <ilhamazalmad11@gmail.com>
2023-03-30 14:38:43 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1 : first param
+ * @s2 : second param
+ *
+ * Return: the comparaison
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
