26c6aec08fd7b6284ffa6d4fc9eeccbe4b90c89b
ilhamazalmad <ilhamazalmad11@gmail.com>
2023-04-12 16:57:14 -0700

    oncatenates all the arguments of the  program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,16 +1,41 @@
 #include "main.h"
 
+/**
+ * argstostr -  concatenates all the arguments of your program
+ * @ac: fisrte arg
+ * @av: second arg
+ *
+ * Return: char
+ */
+
 char *argstostr(int ac, char **av)
 {
-    char *s;
-    int i;
+	char *s;
+	int i, j;
+	int width = 0, r = 0;
 
-    if ((ac == 0) || (av == NULL))
-        return (NULL);
-    s = malloc (ac * sizeof(char *));
-    if (s == NULL)
-        return (NULL);
-    for (i = 0; i < ac; i++)
-        s[i] = *av[i];
-    return s;
+	if ((ac == 0) || (av == NULL))
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			width++;
+		}
+	}
+	s = malloc(((ac + width) * sizeof(char)) + 1);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			s[r] = av[i][j];
+			r++;
+		}
+	if (s[r] == '\0')
+		s[r++] = '\n';
+	}
+	return (s);
 }
+
