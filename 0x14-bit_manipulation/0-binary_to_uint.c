dd1929cd8e936d8cdd8a82ba499a26f44c83864c
ilhamazalmad <ilhamazalmad11@gmail.com>
2023-05-04 04:02:19 -0700

    function that converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * pow_num - pow of two numbers
+ * @x: first param
+ * @y: second param
+ *
+ * Return: the pow
+ *
+ */
+
+int pow_num(int x, int y)
+{
+	int power = 1, i;
+
+	for (i = 1; i <= y; ++i)
+	{
+		power = power * x;
+
+	}
+	return (power);
+}
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: first param
+ *
+ * Return: unsigned int or 0
+ *
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum = 0;
+	unsigned int size = strlen(b);
+	int i = 0;
+
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		if (b[i] == '1')
+			sum += (int)pow_num(2, size - i - 1);
+	}
+	return (sum);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+Bit manipulation : bitwise and logical operation
